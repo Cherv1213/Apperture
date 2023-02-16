8702b2e8208930db94d20234116bced96c63be66
Cherv1213 <vlad70052@gmail.com>
2023-02-16 21:27:33 +0200

    345345

--- script/main.js
+++ script/main.js
@@ -1,42 +1,21 @@
 "use strict"
 
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-if(iconMenu){
-   iconMenu.addEventListener("click", function (e){
-      document.body.classList.toggle('_lock');
-      iconMenu.classList.toggle('_active');
-      menuBody.classList.toggle('_active');
-   });
-}
+// const iconMenu = document.querySelector('.menu__icon');
+// const menuBody = document.querySelector('.menu__body');
+// if(iconMenu){
+//    iconMenu.addEventListener("click", function (e){
+//       document.body.classList.toggle('_lock');
+//       iconMenu.classList.toggle('_active');
+//       menuBody.classList.toggle('_active');
+//    });
+// }
 
-if (iconMenu.classList.contains('_active')){
-   document.body.classList.remove('_lock');
-   iconMenu.classList.remove('_active');
-   menuBody.classList.remove('_active');
-}
+// if (iconMenu.classList.contains('_active')){
+//    document.body.classList.remove('_lock');
+//    iconMenu.classList.remove('_active');
+//    menuBody.classList.remove('_active');
+// }
 
-$(document).ready(function(){
-   $('.owl-carousel').owlCarousel({
-       margin:100,
-       loop:true,
-        autoplay:true,
-        autoplayTimeout:1000,
-        autoplayHoverPause:true,
-        responsive:{
-           0:{
-               items:2,
-               margin:20,
-           },
-           500:{
-            margin:50,
-           },
-           1000:{
-               items:5,
-           }
-       }
-   });
-});
 
 
 /*-------------------------------------------------------------------------------------------------*/
@@ -76,3 +55,42 @@ if (animItems.length > 0) {
 		animOnScroll();
 	}, 300);
 }
+
+
+/*-------------------------------------------------------------------------------------------------*/
+
+
+/* Please ❤ this if you like it! */
+
+
+(function($) { "use strict";
+		
+	
+	
+	//Navigation
+
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
+	
+	
+	
+})(jQuery); 
